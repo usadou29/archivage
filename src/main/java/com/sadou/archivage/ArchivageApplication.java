bef055021d1f88ef1e5c3c3b2cab3ef1c4ef812d
Ulrich Sadou <sadou.ulrich@gmail.com>
2025-07-28 23:25:41 +0200

    Initial commit of my project

---
+++ src/main/java/com/sadou/archivage/ArchivageApplication.java
@@ -0,0 +1,13 @@
+package com.sadou.archivage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadou/archivage/ArchivageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadou.archivage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
